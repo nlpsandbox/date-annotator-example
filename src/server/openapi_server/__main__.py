a3bdec484afd13789c842e90074362e06e6d8292
George Kowalski <gkowalski@mcw.edu>
2020-09-21 15:23:00 -0500

    first update
    Logging turned on along with update to openapi.yaml for post

--- src/server/openapi_server/__main__.py
+++ src/server/openapi_server/__main__.py
@@ -3,15 +3,35 @@
 import connexion
 
 from openapi_server import encoder
+from logging.config import dictConfig
+
 
 
 def main():
+    # Set up logging
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    })
     app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder
+    app.app.logger.warning("Startup of Server...")
     app.add_api('openapi.yaml',
                 arguments={'title': 'NLP Sandbox Date Annotator API'},
                 pythonic_params=True)
     app.run(port=8080)
+    app.app.logger.warning("Stopping of Server...")
 
 
 if __name__ == '__main__':
--- src/server/openapi_server/controllers/date_controller.py
+++ src/server/openapi_server/controllers/date_controller.py
@@ -4,6 +4,8 @@ import six
 from openapi_server.models.date_annotation import DateAnnotation  # noqa: E501
 from openapi_server.models.note import Note  # noqa: E501
 from openapi_server import util
+from datetime import date
+import logging
 
 
 def dates_read_all(note=None):  # noqa: E501
@@ -16,6 +18,12 @@ def dates_read_all(note=None):  # noqa: E501
 
     :rtype: List[DateAnnotation]
     """
+    print("in dates_read_all ")
+    returnList = []
     if connexion.request.is_json:
         note = [Note.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
+        logging.info(f"NOTE TEXT : { note[0]._text}")
+        returnList.append(DateAnnotation( date_format=None, id=2, created_by="George", created_at=date.today(), updated_by="Demo System", updated_at=date.today()))
+        return returnList
+
     return 'do some magic!'
