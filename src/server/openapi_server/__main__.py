2f21b73bfd2a045349c477f7ab520de1f78f5be8
tschaffter <thomas.schaffter@gmail.com>
2020-09-23 13:43:01 -0700

    Remove openapi.yaml from root folder

--- src/server/openapi_server/__main__.py
+++ src/server/openapi_server/__main__.py
@@ -11,7 +11,7 @@ def main():
     app.add_api('openapi.yaml',
                 arguments={'title': 'NLP Sandbox Date Annotator API'},
                 pythonic_params=True)
-    app.run(port=8080)
+    app.run(port=8081)
 
 
 if __name__ == '__main__':
