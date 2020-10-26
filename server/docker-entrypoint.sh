653ab2d88163f6e52b8e6e05729b7419d4cb7d93
tschaffter <thomas.schaffter@gmail.com>
2020-10-25 17:26:13 -0700

    Complete uwsgi solution

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -3,7 +3,9 @@ set -e
 
 if [ "$1" = 'app' ]; then
     cd ${APP_DIR}
-    exec gosu "${APP_UID}" uwsgi --ini app.ini
+
+    # if [ "${APP_}"]
+    exec gosu www-data uwsgi --ini app.ini
 fi
 
 exec "$@"
