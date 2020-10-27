811f8fbb4a86fc43cd6e5a4288185683a04d9072
Thomas Schaffter <thomas.schaffter@gmail.com>
2020-10-26 19:40:02 -0700

    Start server in production mode (#33)
    * Add uwsgi.ini
    
    * Add uwsg and nginx config, and update Dockerfile
    
    * App now works using nginx and uwsgi
    
    * Hide nginx version and run nginx on port 8080
    
    * Remove nginx
    
    * Cleanup server (WIP)
    
    * Complete uwsgi solution
    
    * Update uwsgi config
    
    * Now expect the docker container to run the service with uwsgi
    
    * Update uwdgi config
    
    * Switch to --http-socket
    
    * Remove werkzeug dependency
    
    * Fix tox issue after removing setup.py
    
    * Simplify uwsgi config
    
    * Remove APP_PROCESSES and APP_THREADS
    
    * Add comment to Dockerfile

---
+++ server/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$1" = 'uwsgi' ]; then
+    cd ${APP_DIR}
+    exec gosu www-data "$@"
+fi
+
+exec "$@"
--- server/openapi_server/__main__.py
+++ server/openapi_server/__main__.py
@@ -4,13 +4,15 @@ import connexion
 
 from openapi_server import encoder
 
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml',
+            arguments={'title': 'NLP Sandbox Date Annotator API'},
+            pythonic_params=True)
+
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'NLP Sandbox Date Annotator API'},
-                pythonic_params=True)
+    # TODO: Consider using param host="0.0.0.0", debug=True,
     app.run(port=8080)
 
 
