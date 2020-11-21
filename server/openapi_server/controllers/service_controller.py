4390a039037bad1e61bff9698b98ed380bcca6e3
tschaffter <thomas.schaffter@gmail.com>
2020-11-21 10:13:43 -0800

    Update reference to GH repo returned by the service endpoint

--- server/openapi_server/controllers/service_controller.py
+++ server/openapi_server/controllers/service_controller.py
@@ -15,14 +15,12 @@ def service():  # noqa: E501
         name="date-annotator-example",
         version="0.2.1",
         license="Apache-2.0",
-        repository="github:Sage-Bionetworks/nlp-sandbox-date-annotator-" +
-                   "example",
+        repository="github:nlpsandbox/date-annotator-example",
         description="An example implementation of the NLP Sandbox Date " +
                     "Annotator",
         author="The NLP Sandbox Team",
         author_email="thomas.schaffter@sagebionetworks.org",
-        url="https://github.com/Sage-Bionetworks/nlp-sandbox-date-" +
-            "annotator-example"
+        url="https://github.com/nlpsandbox/date-annotator-example"
     )
 
     return jsonify(service), 200
