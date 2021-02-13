c60a1ddd7f5bb03af761d21eb19554249f75ddbd
tschaffter <thomas.schaffter@gmail.com>
2021-02-13 14:50:12 -0800

    Add /ui redirection

--- server/openapi_server/__main__.py
+++ server/openapi_server/__main__.py
@@ -9,8 +9,8 @@ app = connexion.App(__name__, specification_dir='./openapi/')
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('openapi.yaml', pythonic_params=True)
 
-# Set / to show the tool information
 app.add_url_rule('/', 'root', lambda: flask.redirect('/api/v1/tool'))
+app.add_url_rule('/ui', 'ui', lambda: flask.redirect('/api/v1/ui'))
 
 
 def main():
